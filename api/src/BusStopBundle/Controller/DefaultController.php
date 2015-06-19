b735bf6fd0a341b5a9eca341bf459c8288107183
Lukasz Malicki <lukasz.malicki@polcode.net>
2015-06-19 11:58:05 +0200

    working on angular

--- api/src/BusStopBundle/Controller/DefaultController.php
+++ api/src/BusStopBundle/Controller/DefaultController.php
@@ -9,14 +9,44 @@ class DefaultController extends Controller
 
     public function getLineTypesAction(Request $Request)
     {
-        $em = $this->getDoctrine()->getManager();
-        $repository = $em->getRepository('BusStopBundle:Line');
-        
-        foreach( $repository->findAll() as $line ) {
-            $lines[ $line->getLinetype()->getName() ][] = array( 'lineNumber'=> $line->getId() );
-        }
-        
+
+        #if($Request->isXmlHttpRequest()) {
+	        $em = $this->getDoctrine()->getManager();
+	        $repository = $em->getRepository('BusStopBundle:Line');
+	        
+	        foreach( $repository->findAll() as $line ) {
+	            $lines[ $line->getLinetype()->getName() ][] = array( 'lineNumber'=> $line->getId() );
+	        }
 
         return new JsonResponse( $lines );
+    }
+
+    public function getMessageAction(Request $Request)
+    {
+        	$dayLimit = ( $Request->request->get('dayLimit') ? $Request->request->get('dayLimit') : 7);
+        	$queryLimit = ( $Request->request->get('queryLimit') ? $Request->request->get('queryLimit') : 5);
+
+    		$date = new \DateTime();
+    		$date->modify("-$dayLimit day");
+
+			$repository = $this->getDoctrine()
+			    ->getRepository('BusStopBundle:Message');
+
+			$query = $repository->createQueryBuilder('m')
+			    ->where('m.date > :date')
+			    ->setParameter('date', $date)
+			    ->orderBy('m.date', 'ASC')
+			    ->setMaxResults($queryLimit)
+			    ->getQuery();
+
+	        
+	        foreach( $query->getResult() as $msg ) {
+	            $messages[ $msg->getDate() ][] = array(
+	            	'short' => $msg->getMessageShort(),
+	            	'long'  => $msg->getMessageFull()
+	            );
+	        }
+
+        return new JsonResponse( $messages );
     }
 }
--- web/src/js/main.js
+++ web/src/js/main.js
@@ -1,4 +1,4 @@
-
+     
 var busStopApp = angular.module('busStopApp', [
     'ngRoute',
     'linesServices'
@@ -7,26 +7,22 @@ var busStopApp = angular.module('busStopApp', [
 // configure our routes
 busStopApp.config(['$routeProvider', function ($routeProvider) {
         $routeProvider
-
-                // route for the home page
                 .when('/', {
                     templateUrl: 'views/home.html',
                     controller: 'homeController'
                 })
-
-                // route for the about page
                 .when('/lines', {
                     templateUrl: 'views/lines.html',
                     controller: 'linesController'
                 })
-
-                // route for the contact page
+                .when('/messages', {
+                    templateUrl: 'views/messages.html',
+                    controller: 'messagesController'
+                })
                 .when('/contact', {
                     templateUrl: 'views/contact.html',
                     controller: 'contactController'
                 })
-
-                // else 404
                 .otherwise("/404", {templateUrl: "partials/404.html", controller: "contactController"});
     }]);
 
@@ -35,12 +31,32 @@ busStopApp.controller('homeController', ['$scope', function ($scope) {
         console.log("homeController");
     }]);
 
-busStopApp.controller('linesController', ['$scope', '$routeParams', 'getLines',
-    function ($scope, $routeParams, getLines) {
+busStopApp.controller('linesController', ['$scope', '$routeParams', '$http', 'getLines',
+    function ($scope, $routeParams, $http, getLines) {
         $scope.loading = true;
-        $scope.lines = getLines.query({busStopId: $routeParams.busStopId});
-        //$scope.loading = false;
+        //$scope.lines = getLines.query({busStopId: $routeParams.busStopId});
+        $http.get('../api/web/getLineTypes')
+            .success(function( data, status, header, config) {
+                $scope.lines = data;
+                $scope.loading = false;
+            })
+            .error(function( data, status, header, config) {
+                console.log( status ); 
+            });
     }]);
+
+busStopApp.controller('messagesController', ['$scope', '$routeParams', '$http', 'getLines',
+    function ($scope, $routeParams, $http, getLines) {
+
+        $scope.loading = true;
+        
+        $http.post('../api/web/getMessage', {dayLimit:1, queryLimit: 2})
+            .success(function( data, status, header, config) {
+                $scope.messages = data;
+                $scope.loading = false;
+            });
+    }]);
+
 
 busStopApp.controller('contactController', ['$scope', function ($scope) {
         $scope.submit = function () {
